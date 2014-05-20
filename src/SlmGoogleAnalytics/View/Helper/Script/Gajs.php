17b218bb2dd24a8cc4bee39ba6e8eb46fe6242e1
Bas Kamer <bas@bushbaby.nl>
2014-05-20 14:28:15 +0200

    Adds support to enable display advertising features

--- src/SlmGoogleAnalytics/View/Helper/Script/Gajs.php
+++ src/SlmGoogleAnalytics/View/Helper/Script/Gajs.php
@@ -82,13 +82,23 @@ class Gajs implements ScriptInterface
 
     protected function getLoadScript()
     {
-        return <<<SCRIPT
+        if (!$this->tracker->getEnableDisplayAdvertising()) {
+            return <<<SCRIPT
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();\n
 SCRIPT;
+        } else {
+            return <<<SCRIPT
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();\n
+SCRIPT;
+        }
     }
 
     protected function getVarCreate()
