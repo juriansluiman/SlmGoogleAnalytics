bf7392671014e60ff54f366d447f884fe2f820a7
Bas Kamer <bas@bushbaby.nl>
2014-05-26 14:20:52 +0200

    use heredoc variable

--- src/SlmGoogleAnalytics/View/Helper/Script/Gajs.php
+++ src/SlmGoogleAnalytics/View/Helper/Script/Gajs.php
@@ -82,23 +82,18 @@ class Gajs implements ScriptInterface
 
     protected function getLoadScript()
     {
-        if (!$this->tracker->getEnableDisplayAdvertising()) {
-            return <<<SCRIPT
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();\n
-SCRIPT;
-        } else {
-            return <<<SCRIPT
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();\n
-SCRIPT;
+        $script = 'google-analytics.com/ga.js';
+        if (true === $this->tracker->getEnableDisplayAdvertising()) {
+            $script = 'stats.g.doubleclick.net/dc.js';
         }
+
+        return <<<SCRIPT
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + '$script';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();\n
+SCRIPT;
     }
 
     protected function getVarCreate()
