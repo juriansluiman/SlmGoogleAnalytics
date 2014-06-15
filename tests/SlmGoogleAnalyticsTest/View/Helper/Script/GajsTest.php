af8fdc9ca9d6fe03d193da404ca8644cc90fe55a
Jurian Sluiman <jurian@soflomo.com>
2014-06-15 10:38:42 +0200

    Fix ga.js test for loading from Google as subdomains are removed

--- tests/SlmGoogleAnalyticsTest/View/Helper/Script/GajsTest.php
+++ tests/SlmGoogleAnalyticsTest/View/Helper/Script/GajsTest.php
@@ -113,7 +113,7 @@ class GajsTest extends TestCase
         $expected = <<<SCRIPT
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();\n
 SCRIPT;
